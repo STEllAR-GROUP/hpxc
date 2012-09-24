db4ddd3ea97475ae4fb05f937553a15ee92f5d9f
Bryce Adelstein-Lelbach <blelbach@cct.lsu.edu>
2012-09-24 02:55:51 +0000

    Some header reorganization.

---
+++ hpxc/config.h
@@ -0,0 +1,14 @@
+//  Copyright (c) 2007-2012 Hartmut Kaiser
+//  Copyright (c) 2011-2012 Bryce Adelstein-Lelbach
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#if !defined(HPXC_CONFIG_23_SEP_2012_0134PM)
+#define HPXC_CONFIG_23_SEP_2012_0134PM
+
+#include <hpxc/config/user_main.h>
+#include <hpxc/config/export_definitions.h>
+
+#endif
+
