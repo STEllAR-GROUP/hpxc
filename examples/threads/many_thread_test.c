bd97031dfdb02131aaa2a36fccf02506ccc111be
Alexander Duchene <aduche4@tigers.lsu.edu>
2013-01-28 12:41:13 -0600

    Possible error with master branch

---
+++ examples/threads/many_thread_test.c
@@ -0,0 +1,58 @@
+#include <hpxc/threads.h>
+#include <stdio.h>
+#include <assert.h>
+
+hpxc_key_t key;
+hpxc_mutex_t counter_lock;
+int counter;
+
+hpxc_mutex_t io_lock;
+
+void* creates_key(void* inputs){
+    return NULL;
+}
+
+void* uses_key(void* inputs){
+    hpxc_mutex_lock(&counter_lock);
+    int num=counter++;
+    hpxc_mutex_unlock(&counter_lock);
+
+    double some_work=1;
+    int spin;
+    for(spin=0;spin<1000000;spin++){
+        some_work=some_work*1.1;
+    }
+
+    int result=num;
+    assert(result==num);
+
+    hpxc_mutex_lock(&io_lock);
+    printf("Expected %d, got %d\n",num,result);
+    hpxc_mutex_unlock(&io_lock);
+    return NULL;
+}
+
+void my_init(){
+    counter=9;
+    counter_lock=HPXC_MUTEX_INITIALIZER;
+    io_lock=HPXC_MUTEX_INITIALIZER;
+
+    hpxc_thread_t threads[1000];
+
+    hpxc_thread_t init;
+    hpxc_thread_create(&init,NULL,creates_key,NULL);
+    hpxc_thread_join(init,NULL);
+
+    int i;
+    for(i=0;i<1000;i++){
+        hpxc_thread_create(&(threads[i]),NULL,uses_key,NULL);
+    }
+    for(i=0;i<1000;i++){
+        hpxc_thread_join(threads[i],NULL);
+    }
+}
+
+int main(int argc,char* argv[]){
+    hpxc_init(my_init,argc,argv);
+    return 0;
+}
