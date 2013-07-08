f41c9932d8011e0c8ea2da48945a22087d54a53a
Steven R. Brandt <sbrandt@cct.lsu.edu>
2013-07-08 17:46:05 -0500

    Updating to include timed_wait. Also, removed various warning messages. Also, reduced size of tests so they run without error on my laptop.

---
+++ examples/threads/counter4.c
@@ -0,0 +1,82 @@
+//  Copyright (c) 2012-2013 Alexander Duchene
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying 
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <hpxc/threads.h>
+#include <stdio.h>
+#include <stdlib.h>
+#if !defined(_MSC_VER)
+#include <unistd.h>
+#include <sys/time.h>
+#endif
+
+#define NTHREADS 10000
+
+int counter = 0;
+
+hpxc_mutex_t mut;
+hpxc_cond_t cond;
+
+void* incr(void* p)
+{
+    hpxc_mutex_lock(&mut);
+    counter++;
+    if(counter == NTHREADS)
+        hpxc_cond_broadcast(&cond);
+    hpxc_mutex_unlock(&mut);
+    return NULL;
+}
+
+void *finish(void *p) {
+    hpxc_mutex_lock(&mut);
+    struct timespec tm;
+    tm.tv_sec = 1;
+    tm.tv_nsec = 0;
+    while(counter < NTHREADS) {
+        hpxc_cond_timedwait(&cond,&mut,tm);
+    }
+    hpxc_mutex_unlock(&mut);
+    printf("counter=%d\n",counter);
+    return NULL;
+}
+
+void my_init()
+{
+    int i;
+    struct timeval tv;
+    struct timezone tz;
+    double t1,t2;
+    hpxc_thread_attr_t attr;
+    hpxc_thread_t fin;
+
+    timerclear(&tv);
+    gettimeofday(&tv,&tz);
+    t1 = tv.tv_sec + 1.0e-6*tv.tv_usec;
+
+    hpxc_mutex_init(&mut,NULL);
+    hpxc_cond_init(&cond,NULL);
+
+    hpxc_thread_attr_init(&attr);
+    hpxc_thread_attr_setdetachstate(&attr,1);
+    for(i=0;i<NTHREADS;i++)
+    {
+        hpxc_thread_t th;
+        hpxc_thread_create(&th,NULL,incr,NULL);
+        hpxc_thread_detach(th);
+    }
+    hpxc_thread_attr_destroy(&attr);
+
+    hpxc_thread_create(&fin,NULL,finish,NULL);
+    hpxc_thread_join(fin,NULL);
+
+    timerclear(&tv);
+    gettimeofday(&tv,&tz);
+    t2 = tv.tv_sec + 1.0e-6*tv.tv_usec;
+    printf("time in seconds=%g\n",t2-t1);
+}
+
+int main(int argc, char* argv[]) {
+    hpxc_init(my_init,argc,argv);
+    return 0;
+}
